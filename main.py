433fa29520f196c41c32e2b736055058edd229b1
R. Kyle Norris <r.kyle.norris@gmail.com>
2024-09-26 22:35:48 -0400

    created main.py mainly for testing now but later may be the central part of building CLI part

---
+++ main.py
@@ -0,0 +1,17 @@
+from sql_comands import convert_tasks_to_df
+from classes import Task
+
+# project: str, title: str, description: str,
+# priority: int, category: str,
+# reference_path: str
+
+
+
+tasks = [
+    Task('project1', 'do stuff', 'do certain stuff', 1, 'test', 'none'),
+    Task('project1', 'do things', 'do certain things', 1, 'test', 'none'),
+    Task('project1', 'do wild', 'do wild stuff', 1, 'test', 'none')
+]
+
+df = convert_tasks_to_df(tasks=tasks)
+print(df.head())
