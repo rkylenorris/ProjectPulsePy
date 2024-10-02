67b5849dfb4aaa1842814f4127c841d8c7e428b7
R. Kyle Norris <r.kyle.norris@gmail.com>
2024-10-02 19:05:53 -0400

    small edits for quality and some testing in main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from classes import Task, convert_tasks_to_df
+from tasks_structure import Task, convert_tasks_to_df
+from initial_setup import enum_from_config, Enum
+
+ProjectKind: Enum = enum_from_config()
+for kind in ProjectKind:
+    print(kind.name)
+    print(kind.value)
+    print("----")
 
 # project: str, title: str, description: str,
 # priority: int, category: str,
@@ -6,11 +13,11 @@ from classes import Task, convert_tasks_to_df
 
 
 
-tasks = [
-    Task('project1', 'do stuff', 'do certain stuff', 1, 'test', 'none'),
-    Task('project1', 'do things', 'do certain things', 1, 'test', 'none'),
-    Task('project1', 'do wild', 'do wild stuff', 1, 'test', 'none')
-]
-
-df = convert_tasks_to_df(tasks=tasks)
-print(df.head())
+# tasks = [
+#     Task('project1', 'do stuff', 'do certain stuff', 1, 'test', 'none'),
+#     Task('project1', 'do things', 'do certain things', 1, 'test', 'none'),
+#     Task('project1', 'do wild', 'do wild stuff', 1, 'test', 'none')
+# ]
+#
+# df = convert_tasks_to_df(tasks=tasks)
+# print(df.head())
--- sql_comands.py
+++ sql_comands.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import sqlite3
-from classes import Task
 
 
-def convert_tasks_to_df(tasks: list[Task]) -> pd.DataFrame:
+def convert_tasks_to_df(tasks: list) -> pd.DataFrame:
     return pd.DataFrame([task.__dict__ for task in tasks])
 
 
 def load_df_to_sqlite(df, db_path, table_name):
     conn = sqlite3.connect(db_path)
-    df.to_sql(table_name, conn, if_exists='replace', index=False)
+    df.to_sql(table_name, conn, if_exists='append', index=False)
